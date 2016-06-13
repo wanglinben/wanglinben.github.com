40b9c00134349ab87d87f7035c07a38aaf9b4ccd
Karida0201 <lu1160705720@qq.com>
2016-06-13 20:46:51 +0800

    hjl commit

--- src/js/flight.js
+++ src/js/flight.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+    $('.carousel').carousel({
+     	interval: 2000
+    })
+    
+    $(function () { 
+    	$("[data-toggle='popover']").popover();
+    });
+   
+});
